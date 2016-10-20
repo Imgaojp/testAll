851884e84e9b0a947cd91b439473f80635813220
Imgaojp <imgaojp@gmail.com>
2016-10-20 19:08:45 +0800

    Java Generic And Collections

---
+++ src/JavaGenericsAndCollections/subTypeTest.java
@@ -0,0 +1,25 @@
+package JavaGenericsAndCollections;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by Imgaojp on 2016/10/20.
+ */
+public class subTypeTest {
+    public static void main(String args[]) throws Exception{
+        List ints = new ArrayList() ;
+        ints.add(1);
+        ints.add(3);
+//        List nums = ints;
+//        nums.add(2.3);
+//        System.out.println(nums);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String sin = in.readLine();
+        ints.add(sin);
+        System.out.println(ints);
+    }
+}
